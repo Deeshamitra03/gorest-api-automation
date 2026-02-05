dff9f6f36398bbe98b29f0c43a9a3e5dd3c7544b
User <user@Practo-ka-MacBook-Pro.local>
2026-02-05 15:17:27 +0530

    code changes done

--- src/test/java/com/company/gorest/tests/UserCrudTest.java
+++ src/test/java/com/company/gorest/tests/UserCrudTest.java
@@ -11,32 +11,29 @@ public class UserCrudTest extends BaseTest {
 
     private UserApiClient userApiClient = new UserApiClient();
 
-    // Crucial: We store the ID here so all tests can share it (Request Chaining)
     private static int userId;
 
-    // STEP 1: Create User
     @Test(priority = 1)
     public void testCreateUser() {
-        // 1. Prepare data (Random email to avoid "Email already taken" error)
+
         String randomEmail = "testuser" + System.currentTimeMillis() + "@gmail.com";
         User newUser = new User("Deesha Mitra", randomEmail, "female", "active");
 
-        // 2. Send Request
         Response response = userApiClient.createUser(requestSpec, newUser);
 
-        // 3. Validations (per Assignment)
+
         Assert.assertEquals(response.getStatusCode(), 201, "Status should be 201 Created");
 
-        // 4. Extract ID for next steps (The "Chaining" part)
+
         userId = response.jsonPath().getInt("id");
         System.out.println("Created User ID: " + userId);
 
-        // Validate other fields
+
         Assert.assertEquals(response.jsonPath().getString("name"), "Deesha Mitra");
         Assert.assertEquals(response.jsonPath().getString("email"), randomEmail);
     }
 
-    // STEP 2: Get User
+
     @Test(priority = 2)
     public void testGetUser() {
         Response response = userApiClient.getUser(requestSpec, userId);
@@ -46,10 +43,10 @@ public class UserCrudTest extends BaseTest {
         Assert.assertEquals(response.jsonPath().getString("name"), "Deesha Mitra");
     }
 
-    // STEP 3: Update User
+
     @Test(priority = 3)
     public void testUpdateUser() {
-        // Change name to "Deesha Updated"
+
         User updateUser = new User("Deesha Updated", "new" + System.currentTimeMillis() + "@gmail.com", "female", "active");
 
         Response response = userApiClient.updateUser(requestSpec, userId, updateUser);
@@ -58,7 +55,7 @@ public class UserCrudTest extends BaseTest {
         Assert.assertEquals(response.jsonPath().getString("name"), "Deesha Updated");
     }
 
-    // STEP 4: Delete User
+
     @Test(priority = 4)
     public void testDeleteUser() {
         Response response = userApiClient.deleteUser(requestSpec, userId);
@@ -66,7 +63,7 @@ public class UserCrudTest extends BaseTest {
         Assert.assertEquals(response.getStatusCode(), 204, "Status should be 204 No Content");
     }
 
-    // STEP 5: Verify Deletion (Negative Test)
+
     @Test(priority = 5)
     public void testVerifyDeletion() {
         Response response = userApiClient.getUser(requestSpec, userId);
